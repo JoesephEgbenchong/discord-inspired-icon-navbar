16991b2f8f9dba29293ed453182adcc956065c01
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2023-06-01 17:11:03 +0200

    Added sidebar icons and tooltips

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { FaFire, FaPoo } from 'react-icons/fa'
+import { BsFillLightningFill, BsPlus } from 'react-icons/bs'
+import SideBarIcon from './SideBarIcon'
+
 
 export default function SideBar() {
   return (
     <div className='absolute top-0 left-0 bg-gray-900 h-screen w-24 flex flex-col text-white shadow-lg'>
-        
+        <SideBarIcon icon={<FaFire size="28" />} />
+        <SideBarIcon icon={<BsPlus size="32" />} />
+        <SideBarIcon icon={<BsFillLightningFill size="20" />} />
+        <SideBarIcon icon={<FaPoo size="20" />} />
     </div>
   )
 }
---
+++ src/SideBarIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SideBarIcon( { icon, text= 'tooltip 💡' }) {
+  return (
+    <div className='sidebar-icon group'>
+        {icon}
+
+        <span className='sidebar-tooltip group-hover:scale-100'>{text}</span>
+    </div>
+  )
+}
